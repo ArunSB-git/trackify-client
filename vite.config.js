4f90a0bc419d9b8ec26582480390f2584e46523f
arun-newstreettech <arunatnewstreettech@gmail.com>
2025-12-30 15:44:19 +0530

    test no{2}:vite.config.json

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,23 @@ import react from '@vitejs/plugin-react'
 // });
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,          // listen on all network interfaces
+    strictPort: false,   // don’t fail if port is in use locally
+    port: 5174           // optional local dev port
+  },
+  preview: {
+    host: true, 
+    port: process.env.PORT || 4173,               // use Render’s dynamic port
+    allowedHosts: ['trackify-client-wy2i.onrender.com'] // whitelist Render host
+  },
+  build: {
+    chunkSizeWarningLimit: 1000 // optional: avoid large chunk warnings
+  }
 })
